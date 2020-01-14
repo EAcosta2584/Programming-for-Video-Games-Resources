70597d3e742c6a7fd9a522ec5d4d77e7fd012e02
Viewpoint School <34718652+Team1437@users.noreply.github.com>
2020-01-14 08:35:58 -0800

    Updated with class notes and a moving text and circle.

--- PyGameAnimations.py
+++ PyGameAnimations.py
@@ -2,7 +2,7 @@
 # File Name:        PyGameAnimations
 # Creator Name:     Mr. Acosta
 # Date Created:     1-10-2020
-# Date Modified:    1-10-2020
+# Date Modified:    1-14-2020
 ################################################
 # This program will demonstrate moving shapes in
 # PyGame
@@ -11,13 +11,15 @@
 import pygame, sys, time
 from pygame.locals import *
 
-# Set up pygame.
+# Set up pygame. to run pygame, we must always initialize it.
 pygame.init()
 
-# Set up the window.
+# Here we create the window. We store the window height and width in variables so we can use them later.
 width = 600
 height = 600
 windowSurface = pygame.display.set_mode((width, height), 0, 32)
+
+# Set the window title to "Animation"
 pygame.display.set_caption('Animation')
 
 # Set up direction variables.
@@ -33,35 +35,38 @@ upRight = 'upright'
 
 movementSpeed = 4
 
-# Set up the colors.
+# Set up the color variables.
 black = (0, 0, 0)
 white = (255, 255, 255)
 red = (255, 0, 0)
 green = (0, 255, 0)
 blue = (0, 0, 255)
 
-basicFont = pygame.font.SysFont("magneto", 20)
+# Create up a font object.
+basicFont = pygame.font.SysFont("Comic Sans MS", 20)
 
-myText = "FBI Warning"
+myText = "Hello"
 
-text = basicFont.render(myText,True, white, blue)
+text = basicFont.render(myText,True, white, black)
 
+
+# Get the rectangle of the text and start it in the center of the window.
 textRect = text.get_rect()
-
 textRect.centerx  = windowSurface.get_rect().centerx
 textRect.centery = windowSurface.get_rect().centery
 
 # Set up the box data structure.
 blueSquare = {'rect':pygame.Rect(275,275, 50, 50), 'color':blue, 'dir':upRight}
-redRect = {'rect':pygame.Rect(230, 120, 50, 80), 'color': red, 'dir':downRight}
-textBox =
-boxes = [blueSquare, redRect]
+greenRect = {'rect':pygame.Rect(230, 120, 50, 80), 'color': green, 'dir':downRight}
+textBox = {'rect': textRect, 'color': white, 'dir': upLeft}
+boxes = [blueSquare, greenRect, textBox]
 
-
+x = 30
+circleMovement = 5
 
 # Run the game loop.
 while True:
-    # Check for the QUIT event.
+    # Check for the QUIT (window close) event.
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
@@ -70,11 +75,8 @@ while True:
     # Draw the white background onto the surface.
     windowSurface.fill(white)
 
-    if textRect.centerx > width:
-
-
     for b in boxes:
-        # Move the box data structure.
+        # Move the box data structure base on its stored direction.
         if b['dir'] == downLeft:
             b['rect'].left -= movementSpeed
             b['rect'].top += movementSpeed
@@ -87,6 +89,8 @@ while True:
         if b['dir'] == upRight:
             b['rect'].left += movementSpeed
             b['rect'].top -= movementSpeed
+
+        #The conditionals below are used to check if we hit the edge of the screen.
 
         if b['rect'].right > width:
             if b['dir'] == upRight:
@@ -112,11 +116,25 @@ while True:
             if b['dir'] == upLeft:
                 b['dir'] = upRight
 
-
         # Draw the box onto the surface.
         pygame.draw.rect(windowSurface, b['color'], b['rect'])
 
-        windowSurface.blit(text, textRect)
+
+    # If the X location of the circle is a radius away from the right edge, reverse direction.
+    if x == width -30:
+        circleMovement = -5
+    # If the X location of the circle is a radius away from the left edge, reverse direction.
+    if x == 30:
+        circleMovement = 5
+
+    # Increase the circle's movement.
+    x += circleMovement
+
+    # Draw the circle on the screen.
+    pygame.draw.circle(windowSurface, black, (x, 400), 30)
+
+    # Draw the text onto the screen.
+    windowSurface.blit(text, textRect)
 
     # Draw the window onto the screen.
     pygame.display.update()
