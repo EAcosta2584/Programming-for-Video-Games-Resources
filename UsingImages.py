0365f6268853851576003e01dfcc02fa33c42d1a
Viewpoint School <34718652+Team1437@users.noreply.github.com>
2020-02-06 09:34:45 -0800

    Updated the using images file with comments and functions

--- UsingImages.py
+++ UsingImages.py
@@ -2,7 +2,7 @@
 # File Name: UsingImages.py
 # Creator Name: Mr. Acosta
 # Date Created: 2-3-2020
-# Date Modified: 2-3-2020
+# Date Modified: 2-6-2020
 ################################################
 # This program will demonstrate using images
 # ################################################
@@ -23,7 +23,6 @@ windowSurface = pygame.display.set_mode((width, height), 0, 32)
 pygame.display.set_caption('images')
 
 movementSpeed = 3
-projectileSpeed = 12
 
 # Set up movement variables.
 moveLeft = False
@@ -31,6 +30,7 @@ moveRight = False
 moveUp = False
 moveDown = False
 grow = False
+shrink = False
 angle = 0
 left = False
 right = False
@@ -44,12 +44,25 @@ red = (255, 0, 0)
 green = (0, 255, 0)
 blue = (0, 0, 255)
 
+
+# Image size
 x = 84
 y = 72
 
+# Create a player the size of the image
 player = pygame.Rect(300 - 42 , 300 - 36, x, y)
+
+# Load in the image we are using.
 playerImage = pygame.image.load('dagger.gif')
 
+# The following image formats can be used with PyGame
+# JPG
+# PNG
+# GIF (non-animated)
+# BMP
+# PCX
+# TGA (uncompressed)
+# TIF
 
 
 while True:
@@ -69,6 +82,8 @@ while True:
             moveRight = True
         if event.key == K_SPACE:
             grow = True
+        if event.key == K_z:
+            shrink = True
     if event.type == KEYUP:
         if event.key == K_UP or event.key == K_w:
             moveUp = False
@@ -80,18 +95,95 @@ while True:
             moveRight = False
         if event.key == K_SPACE:
             grow = False
+        if event.key == K_z:
+            shrink = False
         if event.key == K_ESCAPE:
             pygame.quit()
             sys.exit()
 
+    # move the player up
+    if moveUp == True:
+        player.top -= movementSpeed
+
+        # Here we check which direction the player is already facing, and rotate, or flip accordingly
+
+        # This checks if our object's state is "left"
+        if left == True:
+            # The transform.rotate() function takes an image object and rotates it in degrees.
+            # Note that positive is counter-clockwise, and negative is clockwise. The rotation is also relative
+            # on its current position, not its original position.
+            # pygame.transform.rotate(surface, degrees)
+            playerImage = pygame.transform.rotate(playerImage, -90)
+            # Here we tell the program we are no longer facing left
+            left = False
+        if right == True:
+            playerImage = pygame.transform.rotate(playerImage, 90)
+            right = False
+        if down == True:
+            # The transform.flip() function flips the image object about its horizontal and/or vertical axis
+            # pygame.transform.flip(surface, xbool, ybool)
+            playerImage = pygame.transform.flip(playerImage, False, True)
+            down = False
+        # Here we set the state to "up"
+        up = True
+
+    if moveDown == True:
+        player.top += movementSpeed
+        if left == True:
+            playerImage = pygame.transform.rotate(playerImage, 90)
+            left = False
+        if right == True:
+            playerImage = pygame.transform.rotate(playerImage, -90)
+            right = False
+        if up == True:
+            playerImage = pygame.transform.flip(playerImage, False, True)
+            up = False
+        down = True
+
+    if moveLeft == True:
+        player.left -= movementSpeed
+        if right == True:
+            playerImage = pygame.transform.rotate(playerImage, 180)
+            right = False
+        if up == True:
+            playerImage = pygame.transform.rotate(playerImage, 90)
+            up = False
+        if down == True:
+            playerImage = pygame.transform.rotate(playerImage, -90)
+            down = False
+        left = True
+
+    if moveRight == True:
+        player.right += movementSpeed
+        if left == True:
+            playerImage = pygame.transform.rotate(playerImage, 180)
+            left = False
+        if up == True:
+            playerImage = pygame.transform.rotate(playerImage, -90)
+            up = False
+        if down == True:
+            playerImage = pygame.transform.rotate(playerImage, 90)
+            down = False
+        right = True
+
+    # increase or decrease the X and Y values to scale the image
+    if grow == True:
+        x += 1
+        y += 1
+
+    if shrink == True:
+        x -= 1
+        y -= 1
 
 
     # Draw the white background onto the surface.
     windowSurface.fill(white)
 
+    # The transform.scale takes an image object and adjusts its width (x in this example), and height (y in this example)
+    # pygame.transform.scale(surface, (width, height))
     playerImageStretch = pygame.transform.scale(playerImage, (x, y))
 
-    # Draw the player onto the surface.
+    # Draw the image onto the player surface.
     windowSurface.blit(playerImageStretch, player)
 
     # Draw the window onto the screen.
