fa7c1e6ba5069d557e0a795a018573e93eb8d36a
EAcosta2584 <Ernest.Acosta@viewpoint.org>
2020-02-26 22:18:12 -0800

    Upgrades for our scrolling game

---
+++ UpgradingScrolling.py
@@ -0,0 +1,354 @@
+################################################
+# File Name: UpgradingScrolling.py
+# Creator Name: Mr. Acosta
+# Date Created: 2-26-2020
+# Date Modified: 2-26-2020
+#################################################
+#
+#################################################
+
+import pygame, sys, time, random
+from pygame.locals import *
+
+# Set up pygame. to run pygame, we must always initialize it.
+pygame.init()
+mainClock = pygame.time.Clock()
+
+# Set a variable to run the game when True
+running = False
+
+# Here we create the window. We store the window height and width in variables so we can use them later.
+width = 800
+height = 600
+# We can replace the "0, 32" from previous programs with pygame.FULLSCREEN or pygame.RESIZABLE
+windowSurface = pygame.display.set_mode((width, height), pygame.FULLSCREEN)
+
+
+# Set the window title to "Animation"
+pygame.display.set_caption('scrolling')
+
+#Movement Control
+movementSpeed = 9
+projectileSpeed = 10
+scrollSpeed = 6
+
+shotFrameCounter = 0
+targetFrameCounter = 0
+collisionFrameCounter = 0
+
+shots = []
+targets = []
+
+maxLives = 3
+score = 0
+
+maxTargets = 5
+maxShots = 10
+
+# Set up movement variables.
+moveLeft = False
+moveRight = False
+moveUp = False
+moveDown = False
+shoot = False
+pause = False
+
+# Set up the color variables.
+black = (0, 0, 0)
+white = (255, 255, 255)
+red = (255, 0, 0)
+green = (0, 255, 0)
+blue = (0, 0, 255)
+
+titleFont = pygame.font.SysFont("none", 90)
+font = pygame.font.SysFont("none", 24)
+
+# Image size
+x = 48
+y = 54
+
+# Create a player the size of the image
+player = pygame.Rect(100, 300 - 27, x, y)
+bg = pygame.Rect(0, -100, 10, 10)
+
+
+# Load in the images we are using. Note that the background is in another folder
+# so we must include the folder name when loading the image.
+background = pygame.image.load('images/StarsPattern.png')
+playerImage = pygame.image.load('dagger.gif')
+
+# Our image is normally rotated 90 degrees up, so here we turn it to the right
+playerImage = pygame.transform.rotate(playerImage, -90)
+
+# here we are initializing a Sound object called "pew" to use later
+pew = pygame.mixer.Sound('audio/sfx/laser5.wav')
+playerHit = pygame.mixer.Sound('audio/sfx/boom2.wav')
+targetHit = pygame.mixer.Sound('audio/sfx/boom6.wav')
+playerDead = pygame.mixer.Sound('audio/sfx/boom7.wav')
+
+# sound effects by Devin Watson, dklon on OpenGameArt.org
+
+
+# use mixer.music.load to load in the background music file
+pygame.mixer.music.load('audio/music/Space Fighter Loop.mp3')
+
+# The music.play will start the music. Run this before the loop, or else it will attempt to start
+# with every frame of the game. The first number is how many times it should be repeated. Set to -1 to
+# repeat indefinitely. the second is where the file should start in seconds
+# pygame.mixer.music.play(number of repeats, where to start (seconds)
+pygame.mixer.music.play(-1, 0)
+pygame.mixer.music.set_volume(.2)
+
+# save music file name to a variable for use later
+GameOver = 'audio/music/GameOver.mp3'
+
+# Music from Kevin MaCleod and Argonaut Games
+
+# A function for pausing games
+def pauseGame():
+    while True:
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == KEYDOWN:
+                if event.key == K_ESCAPE:
+                    pygame.quit()
+                return False
+
+# load the previous high score
+f = open("scores.txt", "r")
+highScore = int(f.read())
+f.close()
+
+myText = "Space Game"
+text = titleFont.render(myText, True, white)
+textRect = text.get_rect()
+textRect.centerx = windowSurface.get_rect().centerx
+textRect.centery = windowSurface.get_rect().centery - 100
+
+scoreText = "High Score: " + str(highScore )
+text2 = titleFont.render(scoreText, True, white)
+textRect2 = text2.get_rect()
+textRect2.centerx = windowSurface.get_rect().centerx
+textRect2.centery = windowSurface.get_rect().centery + 50
+
+windowSurface.fill(black)
+windowSurface.blit(background, bg)
+# Display game over text and score
+windowSurface.blit(text, (textRect))
+windowSurface.blit(text2, (textRect2))
+
+pygame.display.update()
+
+# Start the game by setting running to True
+running = not pauseGame()
+
+# Run the game loop.
+while running:
+    # Check for events.
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+        if event.type == KEYDOWN:
+            if event.key == K_UP or event.key == K_w:
+                moveUp = True
+            if event.key == K_DOWN or event.key == K_s:
+                moveDown = True
+            if event.key == K_LEFT or event.key == K_a:
+                moveLeft = True
+            if event.key == K_RIGHT or event.key == K_d:
+                moveRight = True
+            if event.key == K_SPACE:
+                shoot = True
+            if event.key == K_p:
+                pause = True
+        if event.type == KEYUP:
+            if event.key == K_UP or event.key == K_w:
+                moveUp = False
+            if event.key == K_DOWN or event.key == K_s:
+                moveDown = False
+            if event.key == K_LEFT or event.key == K_a:
+                moveLeft = False
+            if event.key == K_RIGHT or event.key == K_d:
+                moveRight = False
+            if event.key == K_SPACE:
+                shoot = False
+            if event.key == K_ESCAPE:
+                pygame.quit()
+                sys.exit()
+
+    windowSurface.fill(black)
+    windowSurface.blit(background, bg)
+    bg.left -= scrollSpeed
+    if bg.left < -800:
+        bg.left = 0
+
+    # pause the game and music
+    if pause == True:
+        pygame.mixer.music.pause()
+        pause = pauseGame()
+    else:
+        pygame.mixer.music.unpause()
+
+    # Set adjust our movement when a direction is True
+    if moveUp == True:
+        if player.top > 0:
+            player.top -= movementSpeed
+    if moveDown == True:
+        if player.bottom < height:
+            player.bottom += movementSpeed
+    if moveLeft == True:
+        if player.left > 0:
+            player.left -= movementSpeed
+    if moveRight == True:
+        if player.right < width:
+            player.right += movementSpeed
+
+    # Random set targets on screen and spaces them so they won't overlap
+    if len(targets) < maxTargets:
+        if targetFrameCounter == 0:
+            targetFrameCounter = 10
+            targets.append(pygame.Rect(width + 20, random.randint(10, height -10), 20, 20))
+
+    # Draw targets on screen
+    for i in range (len(targets)):
+        pygame.draw.rect(windowSurface, red, targets[i])
+        targets[i].left -= movementSpeed
+        if targets[i].colliderect(player):
+            targets[i].right = 0
+            if collisionFrameCounter == 0:
+                # playback player hit sound
+                playerHit.play()
+                collisionFrameCounter = 30
+                maxLives -= 1
+
+
+    # If hit, flash during invincibility
+    if collisionFrameCounter > 0:
+        if collisionFrameCounter % 3 == 0:
+            playerImage.set_alpha(128)
+        else:
+            playerImage.set_alpha(256)
+    else:
+        playerImage.set_alpha(256)
+
+    for target in targets[:]:
+        if target.left < - 20:
+            targets.remove(target)
+
+    # Add a projectile to the shots list, but limit to three shots at a time
+    if shoot == True and (len(shots) < maxShots):
+        if shotFrameCounter == 0:
+            # playback the shooting noise
+            pew.play()
+            shotFrameCounter = 6
+            shots.append(pygame.Rect(player.centerx - 3, player.centery - 3, 6, 6))
+
+    # Draw the shots that have been added to the shots list, and move them up by projectile speed
+    for i in range(len(shots)):
+        pygame.draw.rect(windowSurface, green, shots[i])
+        shots[i].left += projectileSpeed
+        for target in targets[:]:
+            if shots[i].colliderect(target):
+                #playback target hit sound
+                targetHit.play()
+                shots[i].left = 800
+                targets.remove(target)
+                score += 1
+
+    # If the shot passes the screen, remove it from the list.
+    for shot in shots[:]:
+        if shot.right > 820:
+            shots.remove(shot)
+
+    # Redraw our list of targets
+    for i in range(len(targets)):
+        pygame.draw.rect(windowSurface, red, targets[i])
+
+    # Draw the player onto the surface.
+    windowSurface.blit(playerImage, (player.left - 14, player.top - 16))
+
+    #update lives
+    livesText = "Lives: " + str(maxLives)
+    text = font.render(livesText, True, white)
+    windowSurface.blit(text, (0,0))
+
+    #update Score
+    scoreText = "Score: " + str(score)
+    text2 = font.render(scoreText, True, white)
+    windowSurface.blit(text2, (width-100,0))
+
+    # if out of lives, end game
+    if maxLives == 0:
+        # playback player dead sound
+        playerDead.play()
+        running = False
+
+    # Draw the window onto the screen.
+    pygame.display.update()
+
+    # Reset our FrameCounters
+    if shotFrameCounter > 0:
+        shotFrameCounter -= 1
+
+    if targetFrameCounter > 0:
+        targetFrameCounter -= 1
+
+    if collisionFrameCounter > 0:
+        collisionFrameCounter -= 1
+
+    # Set the framerate of the game.
+    mainClock.tick(30)
+
+
+myText = "Game Over"
+text = titleFont.render(myText, True, white)
+textRect = text.get_rect()
+textRect.centerx = windowSurface.get_rect().centerx
+textRect.centery = windowSurface.get_rect().centery - 100
+
+scoreText = "Final Score: " + str(score)
+text2 = titleFont.render(scoreText, True, white)
+textRect2 = text2.get_rect()
+textRect2.centerx = windowSurface.get_rect().centerx
+textRect2.centery = windowSurface.get_rect().centery + 50
+
+# playback GameOver music
+pygame.mixer.music.load(GameOver)
+pygame.mixer.music.set_volume(.3)
+pygame.mixer.music.play(0,0)
+# The sound file is 8 seconds and ends abruptly, so lets fade it out at 7 seconds
+pygame.mixer.music.fadeout(7000)
+
+# save scores to display on the title screen
+if score > highScore:
+    f = open("scores.txt", "w")
+    f.write(str(score))
+    f.close()
+# The Game over loop
+while True:
+    # Check for events.
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+        if event.type == KEYUP:
+            if event.key == K_ESCAPE:
+                pygame.quit()
+                sys.exit()
+
+    # keep the background moving
+    windowSurface.fill(black)
+    windowSurface.blit(background, bg)
+    bg.left -= scrollSpeed
+    if bg.left < -800:
+        bg.left = 0
+
+    # Display game over text and score
+    windowSurface.blit(text, (textRect))
+    windowSurface.blit(text2, (textRect2))
+
+    pygame.display.update()
+    mainClock.tick(30)
